016df8abe461748a9033b928637e12732c272121
ye82 <69617023+ye82@users.noreply.github.com>
2024-09-17 23:55:36 +0800

    测试构造函数

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std ;
+
+
+class Entity {
+public:
+    string m_name;
+    void setName(string name) {
+        m_name = name;
+    }
+    string getName() const {
+        return m_name;
+    }
+};
+
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Entity e = Entity();
+    e.setName("shanghai");
+    cout << e.getName() << std::endl;
     return 0;
 }
